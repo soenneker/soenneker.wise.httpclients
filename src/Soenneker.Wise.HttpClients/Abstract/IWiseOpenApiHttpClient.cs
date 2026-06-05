402105a894fafab4bcd97c9457b69ef3037eb467
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:39:00 -0500

    xml commenting

--- src/Soenneker.Wise.HttpClients/Abstract/IWiseOpenApiHttpClient.cs
+++ src/Soenneker.Wise.HttpClients/Abstract/IWiseOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Wise.HttpClients.Abstract;
 /// </summary>
 public interface IWiseOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
